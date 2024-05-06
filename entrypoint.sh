f33cf81abe7219fb1aa3de33152ca5ca30b4b35d
Yier <yier@x1000c3s0b0n1.hostmgmt2000.cm.asp2a.nscc.sg>
2024-05-06 14:18:08 +0800

    update entrypoint script to accept long options

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,34 @@ set -euo pipefail
 
 [ -d data ] || git clone "https://${HF_USER}:${HF_TOKEN}@huggingface.co/datasets/aisingapore/m3exam" data
 
-python main.py "$@"
-python eval.py "$@"
+args=$(getopt -o m:e: -l main:,eval: -n 'm3exam' -- "$@")
+if [ $? != 0 ]; then
+    echo 'Terminating...' >&2
+    exit 1
+fi
+eval set -- "${args}"
+
+main_args=
+eval_args=
+while true; do
+    case "${1}" in
+        -m | --main )
+            main_args=$(echo ${2} | sed 's/,--/ --/g')
+            shift 2
+            ;;
+        -e | --eval )
+            eval_args=$(echo ${2} | sed 's/,--/ --/g')
+            shift 2
+            ;;
+        -- )
+            shift
+            break
+            ;;
+        * )
+            break
+            ;;
+    esac
+done
+
+python main.py ${main_args}
+python eval.py ${eval_args}
